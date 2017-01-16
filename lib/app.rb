8330aef9ce5385b1a892159796f9a34861adafa6
Rick Clark <rkingsleyclark@gmail.com>
2017-01-16 15:57:57 +0000

    Created index iew

--- lib/app.rb
+++ lib/app.rb
@@ -15,5 +15,5 @@ get '/otherstuff' do
 end
 
 get '/cat' do
-  '<img src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png" alt="A photo of a cat" title="A kitty" style="border: 3px dashed red;padding-right: 50px;">'
+  erb(:index)
 end
